4b6aca7a6e8ba0e1541e7d73dd9646fe3c9b8fd8
reynaldo57 <reynaldo57@hotmail.com>
2022-02-17 23:11:32 -0500

    setima commit

--- lib/screens/admin/login_admin_page.dart
+++ lib/screens/admin/login_admin_page.dart
@@ -73,7 +73,7 @@ class _LoginAdminPageState extends State<LoginAdminPage> {
                       height: 60.0,
                     ),
                     Text(
-                      "Iniciar una  sgfgfgtgtgesion",
+                      "Iniciar una",
                       style: TextStyle(
                           fontSize: 30.0,
                           fontWeight: FontWeight.bold
