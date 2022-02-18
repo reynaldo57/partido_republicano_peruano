f1410d71ff5eaa2505d8d6a2ca994cdaf99e0ac1
reynaldo57 <reynaldo57@hotmail.com>
2022-02-17 22:41:32 -0500

    tercer commit

--- lib/screens/admin/login_admin_page.dart
+++ lib/screens/admin/login_admin_page.dart
@@ -73,7 +73,7 @@ class _LoginAdminPageState extends State<LoginAdminPage> {
                       height: 60.0,
                     ),
                     Text(
-                      "Iniciar una sesion",
+                      "Iniciar una sgtgtgesion",
                       style: TextStyle(
                           fontSize: 30.0,
                           fontWeight: FontWeight.bold
