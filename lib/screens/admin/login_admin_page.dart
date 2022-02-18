83b0575287f13c1f18ecb1fbbe77cdde12696cd2
reynaldo57 <reynaldo57@hotmail.com>
2022-02-18 00:07:08 -0500

    onceavo commit

--- lib/screens/admin/login_admin_page.dart
+++ lib/screens/admin/login_admin_page.dart
@@ -73,7 +73,7 @@ class _LoginAdminPageState extends State<LoginAdminPage> {
                       height: 60.0,
                     ),
                     Text(
-                      "fdsfds fkhg",
+                      "fdsfds 434324",
                       style: TextStyle(
                           fontSize: 30.0,
                           fontWeight: FontWeight.bold
