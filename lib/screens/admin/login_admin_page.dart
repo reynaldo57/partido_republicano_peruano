30b1b87a367b15b2b8b6a1f5e4ae5b9c065fa558
reynaldo57 <reynaldo57@hotmail.com>
2022-02-18 00:03:22 -0500

    decimo commit

--- lib/screens/admin/login_admin_page.dart
+++ lib/screens/admin/login_admin_page.dart
@@ -73,7 +73,7 @@ class _LoginAdminPageState extends State<LoginAdminPage> {
                       height: 60.0,
                     ),
                     Text(
-                      "Iniciar tretretret",
+                      "fdsfds tretretret",
                       style: TextStyle(
                           fontSize: 30.0,
                           fontWeight: FontWeight.bold
