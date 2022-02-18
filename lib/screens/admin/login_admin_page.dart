58265962f84b138dd74e46c83ad397dd93ece2f2
reynaldo57 <reynaldo57@hotmail.com>
2022-02-17 23:34:56 -0500

    octava commit

--- lib/screens/admin/login_admin_page.dart
+++ lib/screens/admin/login_admin_page.dart
@@ -73,7 +73,7 @@ class _LoginAdminPageState extends State<LoginAdminPage> {
                       height: 60.0,
                     ),
                     Text(
-                      "Iniciar una",
+                      "Iniciar sedssio",
                       style: TextStyle(
                           fontSize: 30.0,
                           fontWeight: FontWeight.bold
